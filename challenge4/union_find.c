521f778ce8d9e73b9fce91edfd850181c6b9adef
Slyfer123574 <cardona.nelson.22@gmail.com>
2023-03-06 17:21:10 +1100

    Tested the Find(), FindReplace(), and FindCompress() functions for Challenge 4.

--- challenge4/union_find.c
+++ challenge4/union_find.c
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/* For SIZE_MAX */
+#include <stdint.h>
+
 /* Initialization of index table */
 void init(size_t parent[], size_t parent_size) {
 		/* Set all elements as singletons */
@@ -14,7 +17,7 @@ void init(size_t parent[], size_t parent_size) {
 
 size_t Find(size_t parent[], size_t element) {
 		if (parent[element] != SIZE_MAX)
-				return find(parent, parent[element]);
+				return Find(parent, parent[element]);
 		return element;
 }
 
@@ -35,6 +38,15 @@ size_t FindCompress(size_t parent[], size_t element) {
 		return element;
 }
 
+void Union(size_t parent[], size_t parent_tree, size_t child_tree) {
+		if (Find(parent, parent_tree) == Find(parent, child_tree)) {
+				printf("Invalid union: Both elements are in the same set.\n");
+				return;
+		}
+
+		FindReplace(parent, child_tree, FindCompress(parent, parent_tree));
+}
+
 int main(int argc, char* argv[argc+1]) {
 #ifdef NOTHING
 		/* Check there is at least one argument */
@@ -48,13 +60,25 @@ int main(int argc, char* argv[argc+1]) {
 		
 		init(parent, parent_size);
 #endif
+#define parent_size 20
+		size_t parent[parent_size] = {9, SIZE_MAX, 10, SIZE_MAX, 9, 3, 5, 0, 5, SIZE_MAX, 9, 1, 3, 4, 5, 2, 11, 4, 1, 4};
 
-		size_t parent[12] = {9, SIZE_MAX, 3, SIZE_MAX, 1, 3, 5, 5, 5, SIZE_MAX, 9, 9};
-
-		printf("The roots of each element are:\n");
-		for (size_t i = 0; i < 12; ++i) {
+		printf("The root of each element is:\n");
+		for (size_t i = 0; i < parent_size; ++i) {
 				printf("p[%ld]: %ld", i, Find(parent, i));
-				if (i == 11)
+				if (i < parent_size)
+						printf(", ");
+		}
+		printf("\n");
+
+		FindReplace(parent, 16, 69);
+		FindReplace(parent, 6, 420);
+		FindReplace(parent, 2, 21);
+
+		printf("PSYCH!!! The new index table is:\n");
+		for (size_t i = 0; i < parent_size; ++i) {
+				printf("p[%ld]: %ld", i, parent[i]);
+				if (i < parent_size)
 						printf(", ");
 		}
 		printf("\n");
