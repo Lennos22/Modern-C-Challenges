11077d5f6f1191624b89d1208bb4107204659c07
Slyfer123574 <cardona.nelson.22@gmail.com>
2023-03-09 17:33:33 +1100

    Started Challenge 6.

---
+++ challenge6/linear_algebra.c
@@ -0,0 +1,21 @@
+/* Created by Nelson Cardona
+ * Start Time/Date: 17:26/9-03-23
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* If either vector dimensions aren't the same dimension as dim,
+ * pray you don't get a seg fault XD
+ */
+double dotProduct(double vec1[dim], double vec2[dim], size_t dim) {
+	// Function STUB
+	return 0;
+}
+
+void linearTransform(double matrix[][], double vec, size_t vec_dim) {
+	// Function STUB
+}
+
+int main(int argc, char* argv[argc+1]) {
+	return EXIT_SUCCESS;
+}
