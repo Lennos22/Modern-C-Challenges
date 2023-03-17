10b55d3d81829f17b973c8fd79bc6767d5a76f5e
Slyfer123574 <cardona.nelson.22@gmail.com>
2023-03-17 13:26:15 +1100

    Started Challenge 8. Wish me luck!

---
+++ challenge8/shortest_path.c
@@ -0,0 +1,15 @@
+/* Created by: Nelson Cardona
+ * Start Time/Date: 13:23/17-03-23
+ * Completion Time/Date: 
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Computes the shortest path between two nodes */
+size_t shortestPath(size_t nElem, size_t graph[nElem][nElem], size_t n1, size_t n2) {
+	return 0;
+}
+
+int main(int argc, char* argv[argc+1]) {
+	return EXIT_SUCCESS;
+}
