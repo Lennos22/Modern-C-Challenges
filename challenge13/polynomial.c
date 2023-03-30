381ce46c958536b1a650a1d1f6fc7aad52482258
Slyfer123574 <cardona.nelson.22@gmail.com>
2023-03-30 15:13:37 +1100

    Successfully implemented real polynomial division.

--- challenge13/polynomial.c
+++ challenge13/polynomial.c
@@ -16,18 +16,37 @@ void poly_divnr(polynomial* dividend, polynomial* divisor) {
 		return;
 	}
 	size_t quot_degree = dividend->degree - divisor->degree;
-	double quot_coeff[quot_degree];
-	polynomial quotient = {
-		.degree = quot_degree,
-		.coeff = quot_coeff,
-	};
-	double rem_coeff[quot_degree];
-	polynomial remainder = {
-		.degree = quot_degree,
-		.coeff = rem_coeff,
-	};
+	double quot_coeff[quot_degree+1];
+#ifndef NDEBUG
+polynomial quotient = {
+	.degree = quot_degree,
+	.coeff = quot_coeff,
+};
+#endif
 
-
+	for (size_t i = dividend->degree; i >= divisor->degree; --i) {
+		quot_coeff[i-divisor->degree] = dividend->coeff[i]/divisor->coeff[divisor->degree];
+#ifndef NDEBUG
+printf("quot_coeff[%zu] = %g\n", i-divisor->degree, quot_coeff[i-divisor->degree]);
+#endif
+		for (size_t j = i; j >= i-divisor->degree; --j) {
+			dividend->coeff[j] -= quot_coeff[i-divisor->degree]*divisor->coeff[divisor->degree-(i-j)];
+			if (!j) break;
+		}
+#ifndef NDEBUG
+printf("dividend is now:\n");
+poly_print(dividend);
+#endif
+		if (!i) break;
+	}
+#ifndef NDEBUG
+printf("Final quotient is:\n");
+poly_print(&quotient);
+#endif
+	/* Copy quotient into dividend */
+	dividend->degree = quot_degree;
+	for (size_t i = 0; i <= quot_degree; ++i)
+		dividend->coeff[i] = quot_coeff[i];
 }
 
 void poly_print(polynomial* poly_ptr) {
--- challenge13/realpolyroots.c
+++ challenge13/realpolyroots.c
@@ -20,7 +20,19 @@ int main(int argc, char* argv[argc+1]) {
 		.degree = argc-2,
 		.coeff = in_coeff,
 	};
-	printf("Polynomial input is:\n");
+
+	printf("Polynomial (degree %zu) input is:\n", poly_in.degree);
+	poly_print(&poly_in);
+
+	polynomial test = {
+		.degree = 1,
+		.coeff = (double[2]) {
+			[1] = 1,
+			[0] = 2,
+		},
+	};
+	poly_divnr(&poly_in, &test);
+	printf("Quotient (now degree %zu) is:\n", poly_in.degree);
 	poly_print(&poly_in);
 
 	return EXIT_SUCCESS;
